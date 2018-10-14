24eb41b789589d134a9a8292f9d5707c9866a018
Archit Khullar <akhullar@uncc.edu>
2018-10-14 09:29:12 -0400

    Facebook API integration - messaging working

--- TwilioAPI/controllers/WorkRequestController.js
+++ TwilioAPI/controllers/WorkRequestController.js
@@ -153,30 +153,99 @@ exports.register_workrequest_mobile = function(req, res) {
 
 
 
-// var requestLoop = setInterval(function(){
-//   request({
-//       url: "https://graph.facebook.com/v2.9/EasyConnectDuke/posts?access_token=EAAgu8PurRGYBAJaIey7ICaIJ0rsU7XBZB6QlZAyVNAG4NHBIJLn2iIu86bGtoj0eizpS9TymNY9z20A8w1hCs6XDbcDa0aSXTqk9w9atZByeAIT9rZC7e0lKv4KEIbGHZAzwnqzffn4dCuuZB2iVPx8RQZB4GKega5GVOoNOsPj7GJWd1ctebxE2AmS43i0yg22ZAZAZBi94rkE6nHIExKOqxbhUMxkqV9RhYVvkDHZB6WZANAZDZD",
-//       method: "GET",
-//       timeout: 100000,
-//       followRedirect: true,
-//       maxRedirects: 10
-//   },function(error, response, body){
-//       if(!error && response.statusCode == 200){
-//           console.log('sucess!');
-//           //var data = JSON.parse(response.body[0]);
-//           console.log(response.body[0]);
-//
-//           // JSONObject json = new JSONObject(data);
-//           // String statistics = json.getString("");
-//           // JSONObject name1 = json.getJSONObject("data");
-//           // //String ageJohn = name1.getString("Age");
-//           // console.log(name1);
-//
-//       }else{
-//           console.log('error' + response.statusCode);
-//       }
-//   });
-// }, 5000);
+var requestLoop = setInterval(function(){
+  request({
+      url: "https://graph.facebook.com/v2.9/EasyConnectDuke/posts?access_token=EAAgu8PurRGYBAEGx1gBQ9wsIhL0UwZBV3JlgRkovZAOwQL71eMUbPx2H0SzZArttKalEa9YOnkSgYTKFIiW784VcvL9Qpdc3MxZCtYPMPmqAQ6HwUXw7tdFvTMrFUZCBz8ATezoLDfsPiR516xC0Fi8vBeZBSMFslZAyVUF286cyAZDZD",
+      method: "GET",
+      timeout: 100000,
+      followRedirect: true,
+      maxRedirects: 10
+  },function(error, response, body){
+      if(!error && response.statusCode == 200){
+          console.log('sucess!');
+          //var data = JSON.parse(response.body[0]);
+          //console.log(response.body);
+          const parsedRes = JSON.parse(response.body).data;
+          console.log(parsedRes[0].message);
+          var fb_data = parsedRes[0].message;
+          var n = new workrequest();
+          var split_data = fb_data.split(" ");
+
+          n.from = split_data[0];
+          n.Name = split_data[1];
+          n.zip = split_data[2];
+          n.region = split_data[3];
+          n.skill=split_data[4];
+
+         var fromIndex= fb_data.indexOf(split_data[4])+split_data[4].length+1;
+         var toIndex= fb_data.length;
+
+         n.job=fb_data.slice(fromIndex,toIndex);
+
+         var newworkrequest = new workrequest(n);
+         //console.log(newworkrequest);
+         //console.log(n);
+
+         mongoose.connection.collection('workrequests').findOne({from: n.from, Name: n.Name, zip: n.zip, region: n.region, skill: n.skill, job: n.job})
+ .then(function(doc) {
+        if(!doc){
+
+             newworkrequest.save(function(err, survey) {
+               if (err) {
+                 return res.status(400).send({
+                   message: err, status:'400'
+                 });
+               } else {
+                 var getzip = newworkrequest.zip;
+                 console.log(getzip);
+
+                 handyman.find({
+                     zip: getzip
+                   },{
+                     from: 1,
+                     _id: 0
+                   }
+                   , function(err, data){
+                     if (err) console.log(err);
+                     if(data){
+                       var list = []
+
+                       for (var i = 0; i < data.length; i++) {
+                         var counter = data[i].from;
+                         list.push(counter);
+
+                       }
+                       console.log(list);
+                       var message="Hi! I have a job: "+ newworkrequest.job +" please contact me at: "+newworkrequest.from;
+
+                       list.forEach(function(item,index) {
+
+                         sendData(item,message);
+                         console.log("sent");
+                       })
+
+                     }
+                   });
+                 //return res.json({message: 'survey submitted successfully', status:'200'});
+
+                 }
+             });
+
+           } else {
+             console.log('record already exists');
+             }
+         });
+
+         //console.log(n);
+
+
+
+
+      }else{
+          console.log('error' + response.statusCode);
+      }
+  });
+}, 5000);
 
 
 function sendData(toNumber,message) {
